50f6a8d3053dcb32488fa426273fd39dfe9ee2eb
Scofield <zp_field@163.com>
2017-05-19 15:09:01 +0800

    完成indexedDB增删改查模式

---
+++ js/test01.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2017/5/19.
+ */
+setTimeout(function () {
+    document.getElementById('clock').value = new Date();
+}, 1000);
+console.log('我改变啦~版2.0~~!!!改版成功~~');
